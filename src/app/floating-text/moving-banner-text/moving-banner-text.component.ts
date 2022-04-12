e496bd0803943c4afc4801280a8674f1e013bef9
Chowdary chowdary <surendra@apptrinity>
2022-04-12 20:10:12 +0530

    added routing

--- src/app/floating-text/moving-banner-text/moving-banner-text.component.ts
+++ src/app/floating-text/moving-banner-text/moving-banner-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-moving-banner-text',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moving-banner-text.component.scss']
 })
 export class MovingBannerTextComponent implements OnInit {
+  newTitleElem: any;
+  @ViewChild('titleContainer', { static: true }) public titleContainer: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newTitleElem = document.createElement('div');
+    // this.newTitleElem.innerHTML = `<div style='width: calc(100% - 0%);border: 3px solid black;padding: 25px;'></div>`;
+    // this.titleContainer.nativeElement.add(this.newTitleElem);
+
+    // this.newTitleElem = document.createElement('div');
+    // this.newTitleElem.innerHTML = `<div style='width: calc(100% - 0%);border: 3px solid black;padding: 25px;'></div>`;
+    // this.titleContainer.nativeElement.appendChild(this.newTitleElem);
+    
+      // var maindiv = document.getElementById('container');
+      // for (var x = 0; x < 9; x++) {
+      //   maindiv.innerHTML = `<div style='
+      //   width: calc(100% - 0%);
+      //   border: 3px solid black;
+      //   padding: 25px;' id=${x}></div>`;
+      //   let temp = document.getElementById(x.toString());
+      //   temp.innerHTML = `<div style='
+      //   width: calc(100% - 0%);
+      //   border: 3px solid black;
+      //   padding: 25px;' id=${x - 1}></div>`;
+      //   temp.appendChild(this.newTitleElem)
+      //   console.log(maindiv.innerHTML)
+
+      // }
+
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { HeaderComponent } from './layouts/header/header.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  declarations: [HeaderComponent, SidebarComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent,FooterComponent,SidebarComponent
+    HeaderComponent,SidebarComponent
   ]
 })
 export class SharedModule { }
