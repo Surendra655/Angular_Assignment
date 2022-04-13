f9b1d0392034ea091439215739132c2243a7fe66
Chowdary chowdary <surendra@apptrinity>
2022-04-13 16:39:30 +0530

    added marks

--- src/app/student-marks/student-marks.component.ts
+++ src/app/student-marks/student-marks.component.ts
@@ -7,16 +7,61 @@ import { MockDataService } from '../shared/services/mock-data.service';
   styleUrls: ['./student-marks.component.scss']
 })
 export class StudentMarksComponent implements OnInit {
+  marks: any[] = []
+  tblKeys: any[] = []
+  state = 1
 
   constructor(private _dataServise: MockDataService) { }
 
   ngOnInit(): void {
-    this._dataServise.getStudentMarks().subscribe(res => {
-      console.log(res)
-    })
-  }
-sort(){
-  // console.log(source.sort((a, b) => a.item.toLowerCase() < b.item.toLowerCase() ? 1 : -1))
+    this.getMarks();
+  };
 
-}
+  getMarks() {
+    this._dataServise.getStudentMarks().subscribe(res => {
+      this.marks = res.marks;
+      this.tblKeys = Object.keys(res.marks[0]);
+    })
+  };
+  sort(key) {
+    switch (this.state) {
+      case 1: {
+        this.sortType(key, true);
+        this.state = 2
+        break;
+      }
+      case 2: {
+        this.sortType(key, false);
+        this.state = 3
+        break;
+      }
+      default: {
+        this.getMarks();
+        this.state = 1
+        break;
+      }
+    }
+  }
+  sortType(key, sortOrder: boolean) {
+    if (typeof (key) === typeof (this.marks[0][key])) {
+      this.alphabetsSort(key, sortOrder)
+    } else {
+      this.numericSort(key, sortOrder)
+    }
+  }
+  alphabetsSort(key: any, sortOrder: boolean) {
+    if (sortOrder) {
+      this.marks.sort((a, b) => a[key].toLowerCase() < b[key].toLowerCase() ? 1 : -1)
+    } else {
+      this.marks.sort((a, b) => a[key].toLowerCase() < b[key].toLowerCase() ? -1 : 1)
+    }
+  }
+
+  numericSort(key: any, sortOrder: boolean) {
+    if (sortOrder) {
+      this.marks.sort((a, b) => a[key] < b[key] ? 1 : -1)
+    } else {
+      this.marks.sort((a, b) => a[key] < b[key] ? -1 : 1)
+    }
+  }
 }
