44670d55a201ba3fe81eeec07dd0b8a1588305c7
Chowdary chowdary <surendra@apptrinity>
2022-04-13 20:21:25 +0530

    final commit

--- src/app/dynamic-divs/dynamic-divs.component.ts
+++ src/app/dynamic-divs/dynamic-divs.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamic-divs.component.scss']
 })
 export class DynamicDivsComponent implements OnInit {
-
+  items = []
   constructor() { }
 
   ngOnInit(): void {
+    this.functionThatGetsItems()
+  }
+  functionThatGetsItems(val = 20) {
+    for (let index = 0; index < val; index++) {
+      this.items.push({ id: this.items.length + 1 })
+    }
   }
 
+  ngAfterViewInit() {
+    window.onscroll = (ev) => {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        this.functionThatGetsItems(5)
+      }
+    };
+  }
+  divEvent(i){
+    alert(`Button in ${i} Div clicked`)
+  }
 }
